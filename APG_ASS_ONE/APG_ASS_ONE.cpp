275aa0d640b64f63eb1a8c5fa3baf86717095eb0
WillyWonker69 <bravekahweka@gmail.com>
2021-05-31 13:16:45 +0200

    Update APG_ASS_ONE.cpp

--- APG_ASS_ONE/APG_ASS_ONE.cpp
+++ APG_ASS_ONE/APG_ASS_ONE.cpp
@@ -8,6 +8,23 @@
 
 using namespace std;
 
+//Check Number Before Entering
+
+double check_number(string str)
+{
+
+    for (int i = 0; i < str.length(); i++)
+        if (isdigit(str[i]) == false)
+            return -1;
+
+    stringstream geek(str);
+    double num;
+
+    geek >> num;
+
+    return num;
+}
+
 int main()
 {
 }
